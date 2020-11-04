37eaf10b5e68804a05cb32be8d472e207c5449a6
Taha Jamshidi <tahajamshidi58@gmail.com>
2020-11-04 17:19:27 +0330

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { ExampleComponent } from 'countdown-react'
+import CountDown from 'countdown-react'
 import 'countdown-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [run, setRun] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [hours, setHours] = useState(0)
+  return (
+    <div>
+      Seconds:<input value={seconds} onChange={e => setSeconds(e.target.value)}/>
+      Minutes:<input value={minutes} onChange={e => setMinutes(e.target.value)}/>
+      Hours:<input value={hours} onChange={e => setHours(e.target.value)}/>
+
+      <button onClick={e => setRun(true)}>Start</button>
+      <button onClick={e => setRun(false)}>Pause</button>
+      <CountDown  s={seconds} m={minutes} h={hours} run={run} setRun={setRun} />
+    </div>
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const CountDown = ({ s, m, h, run, setRun }) => {
+  const [time, setTime] = useState(0)
+  const [seconds, setSecconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [hours, setHours] = useState(0)
+
+  useEffect(() => {
+    let sec = s
+    let min = m
+    let hou = h
+    if (!s) sec = 0
+    if (!m) min = 0
+    if (!h) hou = 0
+
+    setTime(parseInt(hou) * 3600 + parseInt(min) * 60 + parseInt(sec))
+  }, [s, m, h])
+  const secondsToTime = (secs) => {
+    const hours = Math.floor(secs / (60 * 60))
+
+    const divisorForMinutes = secs % (60 * 60)
+    const minutes = Math.floor(divisorForMinutes / 60)
+
+    const divisorForSeconds = divisorForMinutes % 60
+    const seconds = Math.ceil(divisorForSeconds)
+
+    setSecconds(seconds)
+    setMinutes(minutes)
+    setHours(hours)
+  }
+  useEffect(() => {
+    if (run && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+      if (time === 1) {
+        setRun(false)
+      }
+    } else {
+      setRun(false)
+    }
+    secondsToTime(time)
+  })
+
+  return (
+    <div>
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    </div>
+  )
 }
+
+export default CountDown
